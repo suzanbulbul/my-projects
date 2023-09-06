20918edf3a0bc8c5937ee6d353c36e16083126fe
Your Name <you@example.com>
2023-09-06 15:44:07 +0300

    Dev: added project and new design

---
+++ data.js
@@ -0,0 +1,31 @@
+const projects = [
+    {
+      title: "Feel Track",
+      description: "FeelTrack is an application that allows users to record their daily emotional states and visualize them with monthly and yearly graphs. This platform provides a user-friendly tool for tracking and improving emotional well-being.",
+      technologiesUsed: "NextJS & Redux & Firebase & SCSS & Responsive",
+      liveURL: "https://feeltrack.vercel.app",
+      githubRepository: "https://github.com/suzanbulbul/feeltrack"
+    },
+    {
+      title: "Clothing Web Site",
+      description: "A clothing website developed using Nuxt.js and Strapi, featuring a responsive design for an optimal user experience.",
+      technologiesUsed: "NuxtJS & SCSS & Strapi & Responsive",
+      githubRepository: "https://github.com/suzanbulbul/web-site-ui",
+      githubRepositoryApi: "https://github.com/suzanbulbul/web-site-api"
+    },
+    {
+      title: "Landing Page",
+      description: "This landing page, developed using Vue.js and SCSS, presents an elegant and captivating perfume showcase. With its responsive design, it ensures a seamless user experience across all devices.",
+      technologiesUsed: "VueJS & SCSS & Responsive",
+      liveURL: "https://suzanbulbul.github.io/product-landing-page/tr/",
+      githubRepository: "https://github.com/suzanbulbul/product-landing-page"
+    },
+    {
+      title: "Amazon Clone",
+      description: "During my internship, I worked on developing an Amazon clone project using Vue.js and MongoDB. The goal of this project was to recreate the online shopping experience, providing users with a familiar and immersive e-commerce platform.",
+      technologiesUsed: "VueJS & MongoDB",
+      githubRepository: "https://github.com/suzanbulbul/amazon-clone-admin-ui",
+      githubRepositoryApi: "https://github.com/suzanbulbul/amazon-clone-client-ui",
+    }
+  ];
+  
