5014965ac56e16b6994e32ceb8abdf980648902b
Your Name <you@example.com>
2023-09-22 13:33:05 +0300

    added movies project

--- data.js
+++ data.js
@@ -2,16 +2,23 @@ const projects = [
     {
       title: "Feel Track",
       description: "FeelTrack is an application that allows users to record their daily emotional states and visualize them with monthly and yearly graphs. This platform provides a user-friendly tool for tracking and improving emotional well-being.",
-      technologiesUsed: "NextJS & Redux & Firebase & SCSS & Responsive",
+      technologiesUsed: "NextJS & Redux & Firebase & SCSS & Tailwind & Responsive",
       liveURL: "https://feeltrack.vercel.app",
       githubRepository: "https://github.com/suzanbulbul/feeltrack"
     },
     {
       title: "Clothing Web Site",
       description: "A clothing website developed using Nuxt.js and Strapi, featuring a responsive design for an optimal user experience.",
-      technologiesUsed: "NuxtJS & SCSS & Strapi & Responsive",
+      technologiesUsed: "NuxtJS & SCSS & Strapi & Bootstrap & Responsive ",
       githubRepository: "https://github.com/suzanbulbul/web-site-ui",
       githubRepositoryApi: "https://github.com/suzanbulbul/web-site-api"
+    },
+    {
+      title: "Movies Project",
+      description: "The Movies Project enables users to explore a movie list, add films to their cart, and remove items from the cart as desired.",
+      technologiesUsed: "PureJS & Bootstrap & Responsive",
+      liveURL: "https://movies-project-purejs.vercel.app/",
+      githubRepository: "https://github.com/suzanbulbul/movies-project-purejs"
     },
     {
       title: "Landing Page",
