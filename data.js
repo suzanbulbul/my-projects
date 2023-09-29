6fe0f4b7496126889d832e2b7ef138d5af57d6ea
Your Name <you@example.com>
2023-09-29 11:36:48 +0300

    Dev: changed design

--- data.js
+++ data.js
@@ -2,30 +2,56 @@ const projects = [
     {
       title: "Feel Track",
       description: "FeelTrack is an application that allows users to record their daily emotional states and visualize them with monthly and yearly graphs. This platform provides a user-friendly tool for tracking and improving emotional well-being.",
-      technologiesUsed: "NextJS & Redux & Firebase & SCSS & Tailwind & Responsive",
+      technologiesUsed: "ReactJS/ NextJS & Redux & Firebase & SCSS & Tailwind & Responsive",
+      img: "../assets/img/feeltrack-img.png",
       liveURL: "https://feeltrack.vercel.app",
       githubRepository: "https://github.com/suzanbulbul/feeltrack"
     },
     {
-      title: "Clothing Web Site",
-      description: "A clothing website developed using Nuxt.js and Strapi, featuring a responsive design for an optimal user experience.",
-      technologiesUsed: "NuxtJS & SCSS & Strapi & Bootstrap & Responsive ",
-      githubRepository: "https://github.com/suzanbulbul/web-site-ui",
-      githubRepositoryApi: "https://github.com/suzanbulbul/web-site-api"
+      title: "Invoice Project",
+      description: "This invoicing application includes core functionalities such as creating new invoices, adding invoice line items, and attaching notes. Furthermore, it provides the ability to set due dates, view overdue invoices, and offers an international user experience (i18n). It also comes with a 'dark mode' feature that can be enabled based on user preferences.",
+      technologiesUsed: "ReactJS/ NextJS & Firebase & SCSS & Bootstrap & Responsive & i18n ",
+      img: "../assets/img/invoice-img.png",
+      liveURL: "https://invoice-project-nextjs.vercel.app/",
+      githubRepository: "https://github.com/suzanbulbul/invoice-project-nextjs"
+    },
+    {
+      title: "Wofdex Cripto",
+      description: "Wofdex, developed during my first job experience, is a user-friendly crypto application designed for easy management and tracking of your digital assets. With this app, you can monitor your crypto portfolio in real-time, stay up-to-date with the latest prices, and review your transaction history.",
+      technologiesUsed: "VueJS/ NuxtJS & Strapi & SCSS & Bootstrap & Responsive & i18n ",
+      img: "../assets/img/wofdex-img.png",
+      liveURL: "https://wofdex.com/tr/",
     },
     {
       title: "Movies Project",
       description: "The Movies Project enables users to explore a movie list, add films to their cart, and remove items from the cart as desired.",
-      technologiesUsed: "PureJS & Bootstrap & Responsive",
+      technologiesUsed: "Javascript/ PureJS & Bootstrap & Responsive",
+      img: "../assets/img/movies-img.png",
       liveURL: "https://movies-project-purejs.vercel.app/",
       githubRepository: "https://github.com/suzanbulbul/movies-project-purejs"
+    },
+    {
+      title: "News Portal",
+      description: "News Panel is a news application that provides fast and easy access to the desired news, with the ability to add and update news through an admin panel.",
+      technologiesUsed: "ReactJS/ NextJS & Axios & SCSS & Bootstrap & Responsive",
+      img: "../assets/img/news-img.png",
+      liveURL: "https://news-portal-next-js.vercel.app/",
+      githubRepository: "https://github.com/suzanbulbul/news-portal-nextJS"
     },
     {
       title: "Landing Page",
       description: "This landing page, developed using Vue.js and SCSS, presents an elegant and captivating perfume showcase. With its responsive design, it ensures a seamless user experience across all devices.",
       technologiesUsed: "VueJS & SCSS & Responsive",
+      img: "../assets/img/landingPage-img.png",
       liveURL: "https://suzanbulbul.github.io/product-landing-page/tr/",
       githubRepository: "https://github.com/suzanbulbul/product-landing-page"
+    },
+    {
+      title: "Clothing Web Site",
+      description: "A clothing website developed using Nuxt.js and Strapi, featuring a responsive design for an optimal user experience.",
+      technologiesUsed: "VueJS/ NuxtJS & Strapi & SCSS & Bootstrap & Responsive ",
+      githubRepository: "https://github.com/suzanbulbul/web-site-ui",
+      githubRepositoryApi: "https://github.com/suzanbulbul/web-site-api"
     },
     {
       title: "Amazon Clone",
